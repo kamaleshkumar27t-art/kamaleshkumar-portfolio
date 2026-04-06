daed98746189c5166be1335391d3c1f90d085fcc
kamaleshkumar27t-art <kamaleshkumar27t@gmail.com>
2026-04-06 08:32:29 +0530

    Fix GitHub Pages blank issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -12,6 +11,10 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+
+  
+  base: "/kamaleshkumar-portfolio/",
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
